afe2be4d741349e79912dacc886fd8300ef60061
Antony Dev. <128323771+Antony-Delavi@users.noreply.github.com>
2025-03-29 11:57:00 -0300

    Add files via upload
    Desafio concluido

---
+++ trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Nokia");
+Nokia nokia = new Nokia(numero: "12348412", modelo: "Minizinha", imei: "120984371", memoria: 124);
+nokia.Ligar();
+nokia.InstalarAplicativo("Linkedin");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("SIphone");
+Iphone iphone = new Iphone(numero: "12348412", modelo: "Minizinha", imei: "1201", memoria: 124);
+iphone.Ligar();
+iphone.InstalarAplicativo("Youtube");
